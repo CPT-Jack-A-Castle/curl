90ed2e9c5d0f72acd51b8e7a4d0e6b90a535d35d
Daniel Stenberg <daniel@haxx.se>
2022-06-25 11:02:37 +0200

    fixup fopen.c for the conditional build

--- lib/fopen.c
+++ lib/fopen.c
@@ -24,7 +24,7 @@
 
 #include "curl_setup.h"
 
-#if !defined(CURL_DISABLE_COOKIES) && !defined(CURL_DISABLE_ALTSVC) &&  \
+#if !defined(CURL_DISABLE_COOKIES) || !defined(CURL_DISABLE_ALTSVC) ||  \
   !defined(CURL_DISABLE_HSTS)
 
 #ifdef HAVE_FCNTL_H
